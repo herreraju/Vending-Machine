0301ed83783762856c455d7d0b91cc6e6e1149dd
casey.laughlin <casey.laughlin@MU-C3-03.mullen.pvt.k12.co.us>
2014-10-14 14:02:17 -0600

    vending machine

---
+++ Vending Machine/src/Product.java
@@ -0,0 +1,25 @@
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+
+public class Product
+	{
+	private String name;
+	private int[] price;
+	
+
+	public Product(String n, int[] p)
+		{
+		n = name;
+		p = price;
+		}
+	public String getName()
+		{
+		return name;
+		}
+
+	public int[] getPrice()
+		{
+		return price;
+		}
+	}
