679c80c8d59084f10bda6beb37e242222d2aee8d
casey.laughlin <casey.laughlin@MU-C3-03.mullen.pvt.k12.co.us>
2014-10-14 14:57:40 -0600

    ProductManager

--- Vending Machine/src/ProductManager.java
+++ Vending Machine/src/ProductManager.java
@@ -1,5 +1,36 @@
-
 public class ProductManager
 	{
+	static int quantity;
+	static boolean vendItem = true;
+	static String product;
 
+	public ProductManager(int[] p, int q)
+		{
+		q = quantity;
+		p = Product.price;
+		}
+
+	public int getQuantity()
+		{
+		return quantity;
+		}
+
+	public static void incrementItemCode()
+		{
+		for (int i = 0; i <= 10; i++)
+			{
+			
+			}
+		}
+	public static boolean vendItem() 
+		{
+		if(quantity < 0)
+			{
+			return false;
+			}
+		else 
+			{
+			return true;
+			}
+		}
 	}
